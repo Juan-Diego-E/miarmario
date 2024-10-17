5b3006af5621fc3e9366e78ff15af36c419b6333
Juan-Diego-E <juanelissalde2002@gmail.com>
2024-10-17 17:57:20 -0300

    Adding layout on app file + changed file to jsx

---
+++ miarmario/src/App.jsx
@@ -0,0 +1,13 @@
+import Layout from './features/layout.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Layout />
+      <h1>Testing Front-End</h1>
+    </>
+  );
+}
+
+export default App;
