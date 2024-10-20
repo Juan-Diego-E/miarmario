57d783f133507efa7dec341a71daed017213942a
Juan-Diego-E <juanelissalde2002@gmail.com>
2024-10-19 23:43:23 -0300

    Creating loader

---
+++ miarmario/src/features/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/components/Loader.css';
+
+const Loader = () => {
+    return (
+        <div className="modal-backdrop">
+            <span className="loader"></span>
+        </div>
+    );
+};
+
+export default Loader;
