e34e1f37ce80fa3b42cc1ecf70f438ec307c6cce
Juan-Diego-E <juanelissalde2002@gmail.com>
2024-10-19 23:47:00 -0300

    Testing Loader Animation while sending data

--- miarmario/src/features/pages/LogIn.jsx
+++ miarmario/src/features/pages/LogIn.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { AppContext } from '../store/AppContext';
 
 const LogIn = () => {
+    const { setIsLoading } = useContext(AppContext);
+    const [counter, setCounter] = React.useState(5);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [setIsLoading]);
+
     return (
         <>
             <h1>Iniciar Sesión</h1>
@@ -20,7 +39,10 @@ const LogIn = () => {
                 <button type="submit">Ingresar</button>
 
                 <p>Probar otro método</p>
-                <div className="o">o</div>
+                <br />
+                <div className="o">------------------------ O ------------------------</div>
+                <br />
+                <button type="button">Ingresar con Google</button>
             </form>
         </>
     )
