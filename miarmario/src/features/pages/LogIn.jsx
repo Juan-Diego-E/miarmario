87a3c0457a405a9c02d6fc8c536c2ac66e629461
Juan-Diego-E <juanelissalde2002@gmail.com>
2024-10-21 17:48:56 -0300

    Completing Login Structure

--- miarmario/src/features/pages/LogIn.jsx
+++ miarmario/src/features/pages/LogIn.jsx
@@ -5,30 +5,50 @@ import '../../styles/components/LogIn.css'
 const LogIn = () => {
     return (
         <>
-            <Link to="/">
-                <button className='home-icon'>
+            {/* <button className='home-icon'>
                     <i className="fa-solid fa-house"></i>
+                </button> */}
+            <header>
+                <Link to="/">
+                    <h2 className='logo-title'>mIArmario</h2>
+                </Link>
+                <button className='bars'>
+                    <div className='bar'></div>
+                    <div className='bar'></div>
+                    <div className='bar'></div>
                 </button>
-            </Link>
-            <h1 className='login-title'>Iniciar Sesión</h1>
+            </header>
             <form action='login' className='login-form'>
+                <h1 className='login-title'>Iniciar Sesión</h1>
                 <input
+                    className='email'
                     type="email"
                     name='email'
-                    placeholder='Tu Correo...'
+                    placeholder='Correo'
                     required />
 
                 <input
+                    className='password'
                     type="password"
                     name='password'
-                    placeholder='Contraseña...'
+                    placeholder='Contraseña'
                     required />
 
-                <button type="submit">Ingresar</button>
+                <button className='primary-button' type="submit">Ingresar</button>
 
                 <p>Probar otro método</p>
-                <div className="o">------------------------ O ------------------------</div>
-                <button type="button">Ingresar con Google</button>
+                <div className="o"><div></div> O <div></div></div>
+                <div className='login-options'>
+                    <button className='login-btn' type="button">
+                        <i className="fa-brands fa-google"></i>
+                    </button>
+                    <button className='login-btn' type="button">
+                        <i className="fa-brands fa-apple"></i>
+                    </button>
+                    <button className='login-btn' type="button">
+                        <i className="fa-brands fa-facebook"></i>
+                    </button>
+                </div>
             </form>
         </>
     )
