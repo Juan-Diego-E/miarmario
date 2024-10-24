1673008780990a9aa4916e220ff51341054bf9f2
Juan-Diego-E <juanelissalde2002@gmail.com>
2024-10-23 23:49:45 -0300

    Upgrading Form validations + toggle password view

--- miarmario/src/features/pages/LogIn.jsx
+++ miarmario/src/features/pages/LogIn.jsx
@@ -1,57 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../../styles/components/LogIn.css'
+import '../../styles/components/LogIn.css';
+import ThemeSwitcher from '../components/ThemeSwitcher.jsx';
 
 const LogIn = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const Soon = () => {
+        window.alert('Proximamente...')
+    }
+
     return (
         <>
-            {/* <button className='home-icon'>
-                    <i className="fa-solid fa-house"></i>
-                </button> */}
             <header>
                 <Link to="/">
                     <h2 className='logo-title'>mIArmario</h2>
                 </Link>
-                <button className='bars'>
-                    <div className='bar'></div>
-                    <div className='bar'></div>
-                    <div className='bar'></div>
-                </button>
+                <ThemeSwitcher />
             </header>
             <form action='login' className='login-form'>
                 <h1 className='login-title'>Iniciar Sesión</h1>
+
                 <input
                     className='email'
                     type="email"
                     name='email'
                     placeholder='Correo'
-                    required />
+                    required
+                />
 
-                <input
-                    className='password'
-                    type="password"
-                    name='password'
-                    placeholder='Contraseña'
-                    required />
+                <div className='password-container'>
+                    <input
+                        className='password'
+                        type={showPassword ? 'text' : 'password'}
+                        name='password'
+                        placeholder='Contraseña'
+                        minLength='6'
+                        required
+                    />
+                <p className='password-alert'>La contraseña debe tener al menos 6 caracteres</p>
+                    <button
+                        type="button"
+                        className="toggle-password"
+                        onClick={togglePasswordVisibility}
+                    >
+                        {showPassword ? (
+                            <i className="fa-solid fa-eye-slash"></i>
+                        ) : (
+                            <i className="fa-solid fa-eye"></i>
+                        )}
+                    </button>
+                </div>
 
                 <button className='primary-button' type="submit">Ingresar</button>
 
-                <p>Probar otro método</p>
                 <div className="o"><div></div> O <div></div></div>
+                <p>Probar otro método</p>
+                <br />
                 <div className='login-options'>
-                    <button className='login-btn' type="button">
+                    <button onClick={(e) => Soon()} className='login-btn' type="button">
                         <i className="fa-brands fa-google"></i>
                     </button>
-                    <button className='login-btn' type="button">
+                    <button onClick={(e) => Soon()} className='login-btn' type="button">
                         <i className="fa-brands fa-apple"></i>
                     </button>
-                    <button className='login-btn' type="button">
+                    <button onClick={(e) => Soon()} className='login-btn' type="button">
                         <i className="fa-brands fa-facebook"></i>
                     </button>
                 </div>
             </form>
         </>
-    )
-}
+    );
+};
 
 export default LogIn;
